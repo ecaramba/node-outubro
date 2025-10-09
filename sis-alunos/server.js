0de8639fa817542b59a1142243f7da9e8e830ca7
Edir Dumaszak <edir.dumaszak.ciandt@banqi.com>
2025-10-09 20:16:48 -0300

    server

--- sis-alunos/server.js
+++ sis-alunos/server.js
@@ -3,8 +3,13 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function(req, res){
+    res.send("Pagina inicial")
+});
+
 app.get("/contato", function(req, res){
-    res.send("Pagina de contato");
+    console.log( req.accepts("lalala") )
+    res.send("<h1>Pagina de contato</h1>");
 });
 
 app.listen(3000, function(){
