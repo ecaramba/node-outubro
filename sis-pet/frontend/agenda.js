b248d02f1b3cf409117758e5fd11cdccd4344e54
Edir Dumaszak <edir.dumaszak.ciandt@banqi.com>
2025-10-16 20:14:24 -0300

    form de cadastro

--- sis-pet/frontend/agenda.js
+++ sis-pet/frontend/agenda.js
@@ -1,21 +1,53 @@
 // listagem 
 
-$.getJSON('/agenda', function(dados){
+let servicos = [
+    {
+        nome: "Tosa",
+        valor: 120
+    },
+    {
+        nome: "Banho",
+        valor: 100
+    },
+    {
+        nome: "Escova",
+        valor: 80
+    },
+    {
+        nome: "Consulta",
+        valor: 200
+    },
+];
 
-    dados.forEach(function(valor) {
+$(document).ready(function(){
 
-        let data = new Date(valor.datahora);
+    servicos.forEach(function(item, id){
+        $("#servico").append(`<option value="${id}">${item.nome}</option>`);
+    });
 
-        let item = `<label class="list-group-item d-flex gap-3"> 
-            <input class="form-check-input flex-shrink-0"
-            type="checkbox" value="" style="font-size: 1.375em;"> 
-            <span class="pt-1 form-checked-content"> 
-            <strong> ${valor.nome} - ${valor.tutor} </strong> 
-            <small class="d-block text-body-secondary"> 
-            ${data.toLocaleDateString()}
-            </small> </span> </label>`;
+    $.getJSON('/agenda', function(dados){
 
-        $("#agenda").append(item);
+        dados.forEach(function(valor) {
 
-    }); // fim do forEach
-}); //fim do getJson
+            let data = new Date(valor.datahora);
+
+            let item = `<label class="list-group-item d-flex gap-3"> 
+                <input class="form-check-input flex-shrink-0"
+                type="checkbox" value="" style="font-size: 1.375em;"> 
+                <span class="pt-1 form-checked-content"> 
+                <strong> ${valor.nome} - ${valor.tutor} </strong> 
+                <small class="d-block text-body-secondary"> 
+                ${data.toLocaleDateString()}
+                </small> </span> </label>`;
+
+            $("#agenda").append(item);
+
+        }); // fim do forEach
+    }); //fim do getJson
+
+    $("#servico").change(function(ev){
+        let id = this.value;
+        let valor = servicos[id].valor;
+        $("#valor").val(valor);
+    }); //fim do change
+});
